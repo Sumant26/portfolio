84970b0c7450e68651f6c30ce6dfb47e25647de1
Sumant Tulshibagwale <sumanttulshibagwale@gmail.com>
2024-04-23 18:51:18 +0530

    added resume

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,6 +3,15 @@ import "./index.css";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
+  const handleResumeClick = () => {
+    // Your logic for handling LinkedIn icon click
+    window.open(
+      "https://drive.google.com/file/d/1pSZu9APXrVfJYKo5B2o9FlYSRoI5nSq9/view?usp=sharing",
+      "_blank"
+    );
+    console.log("Resume icon clicked");
+  };
+
   return (
     <div className="navbar">
       <span>
@@ -16,7 +25,7 @@ export default function Navbar() {
           <Link to="/About">
             <span className="spanAbout">About</span>
           </Link>
-          <span>Resume</span>
+          <span onClick={handleResumeClick}>Resume</span>
         </span>
         <br />
       </span>
