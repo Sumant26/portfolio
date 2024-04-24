60917df5e551945f91fc4deaa21ec5a85aad5097
Sumant Tulshibagwale <sumanttulshibagwale@gmail.com>
2024-04-24 14:38:03 +0530

    added grid

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header";
 import Tile from "../../components/Tile";
+import "./index.css";
 
 export default function Home() {
   return (
@@ -13,7 +14,28 @@ export default function Home() {
           title="Hey! I'm Sumant Tulshibagwale.
                  My recent work can be viewed here."
         />
-        <Tile title="BGV" description="Build a BGV appition." />
+        <div className="projects">
+          <div className="grid-container">
+            <div className="grid-item-1">
+              <Tile
+                title="BGV"
+                description="Developed a user registration system using controlled React components where individuals can sign up and create their profiles."
+              />
+            </div>
+            <div className="grid-item-2">
+              <Tile
+                title="BGV"
+                description="Developed a user registration system using controlled React components where individuals can sign up and create their profiles."
+              />
+            </div>
+            <div className="grid-item-2">
+              <Tile
+                title="BGV"
+                description="Developed a user registration system using controlled React components where individuals can sign up and create their profiles."
+              />
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
